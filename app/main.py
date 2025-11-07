2d6e410cececc994702e76c79dccc007c6dd96b8
pygod139 <rubinbas7@gmail.com>
2025-11-07 20:33:02 +0545

    fixing

--- app/main.py
+++ app/main.py
@@ -1,16 +1,17 @@
-#$ main entrypoint!
+try:
+    from fastapi import FastAPI
+    from app.routes.login import router
+    from starlette.middleware.sessions import SessionMiddleware
 
-from fastapi import FastAPI
-from app.routes.login import router
-from starlette.middleware.sessions import SessionMiddleware
+    app = FastAPI()
 
+    @app.get("/")
+    async def root():
+        return {"message": "Hello from FastAPI on Vercel!"}
 
-app = FastAPI()
+    app.include_router(router)
+    app.add_middleware(SessionMiddleware, secret_key="nothing-here-lol-its-for-just-using-authlib-testing")
 
-@app.get("/")
-async def root():
-    return {"message": "Hello from FastAPI on Vercel!"}
-
-app.include_router(router)
-app.add_middleware(SessionMiddleware, secret_key="nothing-here-lol-its-for-just-using-authlib-testing")
-
+except Exception as e:
+    print("Startup error:", e)
+    raise e
